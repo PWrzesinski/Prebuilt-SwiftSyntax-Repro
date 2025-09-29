191db6162a568f8d7741626ff1bc51849590d882
Przemysław Wrzesiński <p.wrzesinski@vatio.eu>
2025-09-29 14:22:46 +0200

    Add SwiftLint

--- Package.swift
+++ Package.swift
@@ -3,13 +3,22 @@
 import PackageDescription
 import CompilerPluginSupport
 
+let plugins: [Target.PluginUsage]
+
+#if os(Linux)
+    plugins = []
+#else
+    plugins = [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+#endif
+
 let package = Package(
     name: "Prebuilt SwiftSyntax Repro",
     platforms: [
        .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", exact: "602.0.0")
+        .package(url: "https://github.com/apple/swift-syntax.git", exact: "602.0.0"),
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins.git", exact: "0.61.0")
     ],
     targets: [
         .macro(
@@ -25,7 +34,8 @@ let package = Package(
             dependencies: [
                 .target(name: "PSRMacros")
             ],
-            path: "Sources/App"
+            path: "Sources/App",
+            plugins: plugins
         ),
     ]
 )
